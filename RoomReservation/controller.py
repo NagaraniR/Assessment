9d109f23eb26ff3bacf89842bb60f7b8c89234f7
Nagarani.R <nagaraniraja9677@gmail.com>
2018-01-05 19:04:54 +0530

    second assessment

--- RoomReservation/controller.py
+++ RoomReservation/controller.py
@@ -4,25 +4,37 @@ import datetime
 import calendar
 import operator
 
+
 class Controller():
 
     def __init__(self):
         self.customers_details = []
         self.hotels_details = []
 
+    def add_customers_details(self, customer_details):
+        self.customers_details.append(customer_details)
+
+    def add_hotels_details(self, hotel_hotels):
+        self.hotels_details.append(hotel_hotels)
+        
     def user_input(self):
-        user_dates = []
-        username = raw_input("Enter username")
-        for cutomer in self.customers_details:
-            if username == cutomer.name:
-                days_to_stay=int(raw_input("How many days you want to stay \n"))
-                for i in range(0,days_to_stay):
-                    user_date=raw_input("Enter date in the format dd/mm/yy")
-                    user_dates.append(user_date)
-                break    
-        else:
-            print "Sorry the entered user name is not valid, enter valid"
-        return username, user_dates
+        result = True
+        try:    
+            username = raw_input("Enter username")
+            for cutomer in self.customers_details:
+                if username == cutomer.name:
+                    user_dates = []
+                    days_to_stay=int(raw_input("How many days you want to stay \n"))
+                    for i in range(0,days_to_stay):
+                        user_date=raw_input("Enter date in the format dd/mm/yy")
+                        user_dates.append(user_date)
+                    break
+            else:
+                print "Sorry the entered user name is not valid, enter valid"
+        except Exception as exception:
+            result = False
+            print exception
+        return result, username, user_dates
 
     def convert_dates_to_days(self,user_dates):
         result = True
@@ -37,7 +49,7 @@ class Controller():
             result = False
             print exception
             username, user_dates = controller.user_input()
-            user_booking_days = controller.convert_dates_to_days(user_dates)    
+            user_booking_days = controller.convert_dates_to_days(user_dates)
         return result, user_booking_days
     
     def seperate_days(self, user_booking_days):
@@ -61,55 +73,86 @@ class Controller():
         for customer in self.customers_details:
             if customer.name == user_name:
                 customer_type = customer.cutomer_type
-        return customer_type
+                break
+        return customer
 
-    def get_hotels_rate(self, customer_type, week_days, week_ends):
-        for hotel_details in self.hotels_details:
-            hotel_details.get_hotel_rate(customer_type,week_days, week_ends)
+    def get_hotels_rate(self, customer, week_days, week_ends):
+        result = True
+        try:
+            for hotel_details in self.hotels_details:
+                hotel_details.get_hotel_rate(customer.cutomer_type,week_days, week_ends)
+        except Exception as exception:
+            result = False
+            print exception
+        return result    
 
     #import pdb;pdb.set_trace()
     def cheepest_hotel(self):
-        cheepest_hotel = ""
-        cheepest_rate = self.hotels_details[0].hotel_rate
-        for hotels_detail in self.hotels_details:
-            if hotels_detail.hotel_rate < cheepest_rate:
-                cheepest_rate = hotels_detail.hotel_rate
-                cheepest_hotel = hotels_detail.name
-            else:
-                cheepest_hotel = self.hotels_details[0].name
-        print "Available hotel is ", cheepest_hotel
-        return cheepest_hotel
-            
+        result = True
+        try:
+            cheepest_hotel = ""
+            cheepest_rate = self.hotels_details[0].hotel_rate
+            for hotel_details in self.hotels_details:
+                if hotel_details.hotel_rate < cheepest_rate:
+                    cheepest_rate = hotel_details.hotel_rate
+                    cheepest_hotel = hotel_details
+                else:
+                    cheepest_hotel = self.hotels_details[0]
+        except Exception as exception:
+            result = False
+            print exception            
+        return result, cheepest_hotel
+    
+    def get_available_hotel(self, cheepest_hotel):
+        result = True
+        try:
+            available_hotel = ""
+            for hotel_details in self.hotels_details:
+                if hotel_details.hotel_rate == cheepest_hotel.hotel_rate:
+                    if hotel_details.name != cheepest_hotel.name:
+                        if hotel_details.rating > cheepest_hotel.rating:
+                            available_hotel = hotel_details.name
+                        else:
+                            available_hotel = cheepest_hotel.name
+                    else:
+                        available_hotel = cheepest_hotel.name
+            print  "available_hotel", available_hotel
+        except Exception as exception:
+            result = False
+            print exception 
+        return result
+
+    
 if __name__ == "__main__":
     controller = Controller()
     
     customer1 = Customer("Naga", "Regular")
-    controller.customers_details.append(customer1)
+    controller.add_customers_details(customer1)
     customer2 = Customer("Rani", "Reward")
-    controller.customers_details.append(customer2)
+    controller.add_customers_details(customer2)
     customer3 = Customer("Raja", "Regular")
-    controller.customers_details.append(customer3)
+    controller.add_customers_details(customer3)
     customer4 = Customer("raveena", "Reward")
-    controller.customers_details.append(customer4)
+    controller.add_customers_details(customer4)
     customer5 = Customer("Nithya", "Regular")
-    controller.customers_details.append(customer5)
+    controller.add_customers_details(customer5)
     
     Lakewood = Hotel("Lakewood", 3)
     Lakewood.add_price_details(110, 90, 80, 80)
-    controller.hotels_details.append(Lakewood)
+    controller.add_hotels_details(Lakewood)
 
     Bridgewood = Hotel("Bridgewood", 4)
     Bridgewood.add_price_details(160, 60, 110, 50)
     controller.hotels_details.append(Bridgewood)
 
     Ridgewood = Hotel("Ridgewood", 5)
-    Ridgewood.add_price_details(220, 150, 110, 40)
-    controller.hotels_details.append(Ridgewood)
+    Ridgewood.add_price_details(220, 150, 100, 40)
+    controller.add_hotels_details(Ridgewood)
 
-    username, user_dates = controller.user_input()
+    result, username, user_dates = controller.user_input()
     result, user_booking_days = controller.convert_dates_to_days(user_dates)
     week_days, week_ends = controller.seperate_days(user_booking_days)
-    customer_type = controller.get_customer_type(username)
-    
-    hotels_rate = controller.get_hotels_rate(customer_type, week_days, week_ends)
-    controller.cheepest_hotel()
+    customer = controller.get_customer_type(username)
+    result = controller.get_hotels_rate(customer, week_days, week_ends)
+    result, cheepest_hotel = controller.cheepest_hotel()
+    controller.get_available_hotel(cheepest_hotel)
